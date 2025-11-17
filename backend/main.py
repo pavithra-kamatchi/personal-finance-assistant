4b7cdec7c8c6578e080a48e3300ee00963b56f47
pavithra-kamatchi <pavithra.ks2005@gmail.com>
2025-11-17 11:10:11 -0500

    added budget router and configured the CORS to allow fronted requests

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.api.routes.transactions import transactions_router
 from backend.api.routes.auth import auth_router
 from backend.api.routes.analytics import analytics_router
+from backend.api.routes.budget import budget_router
 from fastapi.openapi.utils import get_openapi
 from fastapi.openapi.models import SecuritySchemeType
 
 app = FastAPI()
 
+# Configure CORS to allow frontend requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 #Include routers
 app.include_router(transactions_router, prefix="/transactions", tags=["transactions"])
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(analytics_router, prefix="/analytics", tags=["analytics"])
+app.include_router(budget_router, prefix="/budget", tags=["budget"])
 
 def custom_openapi():
     if app.openapi_schema:
