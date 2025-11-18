94eff95ee75349d0861b677405389572033da69c
pavithra-kamatchi <pavithra.ks2005@gmail.com>
2025-11-18 12:18:10 -0500

    delete files since implemented React frontend

--- frontend/visualization.js
+++
@@ -1,2 +0,0 @@
-//#Generates simple charts (matplotlib/plotly) or JSON data for frontend.
-//  Creates bar charts, pie charts, line graphs for spending patterns. -  need to convert this to React compatible format.
